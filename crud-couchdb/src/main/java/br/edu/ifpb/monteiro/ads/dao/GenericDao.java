38baee2a7e40b8eaa400abc73f0a0e2fb76fd8d0
Andre Luis <tr.andreluis@gmail.com>
2016-07-27 16:36:51 -0300

    Adicao de um esqueleto para o dao generico

--- crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/dao/GenericDao.java
+++ crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/dao/GenericDao.java
@@ -1,5 +1,9 @@
 package br.edu.ifpb.monteiro.ads.dao;
 
+/**
+ * Dao Generico que ira fornecer as operacoes basicas de insercao, edicao, exclusao e busca no banco.
+ */
+
 public abstract class GenericDao {
 
 	
