15607df3d1952d16dc6b071db1d4d15504d6dcda
Andre Luis <tr.andreluis@gmail.com>
2016-07-27 16:35:32 -0300

    Adicao de um tipo de dado para testes de insercao no banco

---
+++ crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/model/Dado.java
@@ -0,0 +1,19 @@
+package br.edu.ifpb.monteiro.ads.model;
+
+/**
+ * Dado qualquer utilizado para testes iniciais
+ */
+
+public class Dado {
+
+	String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
