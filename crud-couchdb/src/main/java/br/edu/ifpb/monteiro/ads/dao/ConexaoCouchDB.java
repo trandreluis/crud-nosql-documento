a3f75476804138c1146b8fd57a134128c65a9eae
Andre Luis <tr.andreluis@gmail.com>
2016-07-27 20:14:17 -0300

    Implementacao completa do metpd de criacao do banco e obtencao de uma conexao

--- crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/dao/ConexaoCouchDB.java
+++ crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/dao/ConexaoCouchDB.java
@@ -26,6 +26,10 @@ public class ConexaoCouchDB {
 	private static final int NUMERO_MAXIMO_DE_CONEXOES = 10;
 	
 	public ConexaoCouchDB() {
+		
+		/**
+		 * Definindo propriedades da conexao e do banco (nome do banco, endereco IP e porta)
+		 */
 		CouchDbProperties propriedades = new CouchDbProperties();
 		propriedades.setCreateDbIfNotExist(true);
 		propriedades.setDbName("crud_bd2");
@@ -33,8 +37,16 @@ public class ConexaoCouchDB {
 		propriedades.setHost("127.0.0.1");
 		propriedades.setPort(5984);
 		propriedades.setMaxConnections(NUMERO_MAXIMO_DE_CONEXOES);
+		
+		/**
+		 * Estabalecendo conexao e criando o banco caso ele ainda nao exista
+		 */
+		dbClient = new CouchDbClient(propriedades);
+	
 	}
 	
-	
+	public void salvar(String nome) {
+		
+	}
 	
 }
