ddb90218cb4b843f9cef20a7486006da5b9c23bb
Andre Luis <tr.andreluis@gmail.com>
2016-07-27 23:54:09 -0300

    Adicao de comentarios a respeito do metodo salvar() e mudança no nome e tamanho do Dado persistido

--- crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/view/Main.java
+++ crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/view/Main.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.monteiro.ads.view;
 
 import br.edu.ifpb.monteiro.ads.dao.ConexaoCouchDB;
+import br.edu.ifpb.monteiro.ads.model.Dado;
 
 /**
  * Classe principal da aplicacao
@@ -10,7 +11,31 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		ConexaoCouchDB ccdb = new ConexaoCouchDB();
+		/**
+		 * Criando conexao (ao instanciar essa classe seu construtor criara a conexao)
+		 */
+		ConexaoCouchDB couchConnection = new ConexaoCouchDB();
+		
+		/**
+		 * Criando um dado de exemplo
+		 */
+		Dado dado = new Dado();
+		
+		/**
+		 * Definindo os valos deste dado
+		 */
+		dado.setNome("Meu amigo Chalie Brown, Charlie Brown...");
+		dado.setTamanho(78);
+		
+		/**
+		 * Salvando os dados utilizando a conexao criada.
+		 * 
+		 * Aqui o metodo salvar recebe um objeto qualquer.
+		 * Apos receber o objeto, o proprio CouchDB faz o mapeamento
+		 * (pegando seus atributos e os respectivos valores de cada um)
+		 * Apos mapear, o CouchDB persiste (salva) o objeto no banco
+		 */
+		couchConnection.salvar(dado);
 	
 	}
 	
