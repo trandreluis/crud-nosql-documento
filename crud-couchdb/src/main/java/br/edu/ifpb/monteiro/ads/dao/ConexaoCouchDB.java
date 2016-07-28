177d33105fe9097669ed870ff110453172195630
Andre Luis <tr.andreluis@gmail.com>
2016-07-27 23:49:27 -0300

    Adicao de comentarios para melhor entendimento do metodo salvar()

--- crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/dao/ConexaoCouchDB.java
+++ crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/dao/ConexaoCouchDB.java
@@ -3,34 +3,23 @@ package br.edu.ifpb.monteiro.ads.dao;
 import org.lightcouch.CouchDbClient;
 import org.lightcouch.CouchDbProperties;
 
-import br.edu.ifpb.monteiro.ads.model.Dado;
-
 public class ConexaoCouchDB {
 
 	/**
 	 * Pegando instancia de cliente do banco CouchDB
 	 */
 	private static CouchDbClient dbClient;
-	
-	/**
-	 * Definindo o numero maximo de Threads que podem realizar operacoes simultaneas no banco.
-	 */
-	private static final int NUMERO_DE_THREADS = 100;
-	
-	/**
-	 * Definindo o numero maximo de documentos que uma Thread pode manipular
-	 */
-	private static final int DOCUMENTOS_POR_THREAD = 5;
-	
+
 	/**
 	 * Numero maximo de conexoes simultaneas ativas
 	 */
 	private static final int NUMERO_MAXIMO_DE_CONEXOES = 10;
-	
+
 	public ConexaoCouchDB() {
-		
+
 		/**
-		 * Definindo propriedades da conexao e do banco (nome do banco, endereco IP e porta)
+		 * Definindo propriedades da conexao e do banco (nome do banco, endereco
+		 * IP e porta)
 		 */
 		CouchDbProperties propriedades = new CouchDbProperties();
 		propriedades.setCreateDbIfNotExist(true);
@@ -39,16 +28,26 @@ public class ConexaoCouchDB {
 		propriedades.setHost("127.0.0.1");
 		propriedades.setPort(5984);
 		propriedades.setMaxConnections(NUMERO_MAXIMO_DE_CONEXOES);
-		
+
 		/**
 		 * Estabalecendo conexao e criando o banco caso ele ainda nao exista
 		 */
 		dbClient = new CouchDbClient(propriedades);
-	
+
 	}
-	
-	public void salvar(Dado dado) {
+
+	/**
+	 * Metodo salvar que recebe um objeto (Object) qualquer e o mapeia O
+	 * mapeamento é feito pegando os atributos existentes no objeto passado e
+	 * criando um documento com os respectivos campos e valores que os atributos
+	 * vierem a possuir
+	 * 
+	 * @param dado
+	 *            Um tipo de dado qualquer que o metodo espera para persistir
+	 *            (salvar) no banco de dados.
+	 */
+	public void salvar(Object dado) {
 		dbClient.save(dado);
 	}
-	
+
 }
