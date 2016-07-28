2aaa10039ddea67b1df71a2c5262aaff4b46bf91
Andre Luis <tr.andreluis@gmail.com>
2016-07-27 23:55:28 -0300

    Adicao do metodo construtor da View.java para criacao da janela futuramente

--- crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/view/View.java
+++ crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/view/View.java
@@ -2,9 +2,17 @@ package br.edu.ifpb.monteiro.ads.view;
 
 import javax.swing.JFrame;
 
+/**
+ * Classe da janela principal do sistema, que ainda sera feita
+ */
+
 @SuppressWarnings("serial")
 public class View extends JFrame {
 
-	
+	public View() {
+		
+		
+		
+	}
 	
 }
