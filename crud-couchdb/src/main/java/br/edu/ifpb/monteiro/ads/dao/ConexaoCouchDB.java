7ac422b1b18e74c5f566bc353cb33e8072c7746b
Andre Luis <tr.andreluis@gmail.com>
2016-07-30 05:20:16 -0300

    Adicao de comentarios para uma melhor documentacao

--- crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/dao/ConexaoCouchDB.java
+++ crud-couchdb/src/main/java/br/edu/ifpb/monteiro/ads/dao/ConexaoCouchDB.java
@@ -3,24 +3,18 @@ package br.edu.ifpb.monteiro.ads.dao;
 import org.lightcouch.CouchDbClient;
 import org.lightcouch.CouchDbProperties;
 
-import br.edu.ifpb.monteiro.ads.execoes.DadoInexistenteException;
-import br.edu.ifpb.monteiro.ads.execoes.DadoInvalidoException;
-import br.edu.ifpb.monteiro.ads.execoes.DadoSemIdException;
-import br.edu.ifpb.monteiro.ads.execoes.IdDuplicadoException;
-import br.edu.ifpb.monteiro.ads.model.Pessoa;
-
 public class ConexaoCouchDB {
 
 	/**
 	 * Pegando instancia de cliente do banco CouchDB
 	 */
-	private static CouchDbClient dbClient;
+	private CouchDbClient dbClient;
 
 	public ConexaoCouchDB() {
 
 		/**
-		 * Definindo propriedades da conexao e do banco (nome do banco, endereco
-		 * IP e porta)
+		 * Definindo propriedades da conexao e do banco (que ele seja criado caso ainda nao exista, nome do banco,
+		 * protocolo, endereco IP e porta)
 		 */
 		CouchDbProperties propriedades = new CouchDbProperties();
 		propriedades.setCreateDbIfNotExist(true);
@@ -37,79 +31,12 @@ public class ConexaoCouchDB {
 	}
 
 	/**
-	 * Metodo salvar que recebe um objeto (Object) qualquer e o mapeia O
-	 * mapeamento é feito pegando os atributos existentes no objeto passado e
-	 * criando um documento com os respectivos campos e valores que os atributos
-	 * vierem a possuir
+	 * Metodo que retorna um client do CouchDB para os Dao's
 	 * 
-	 * @param dado
-	 *            Um tipo de dado qualquer que o metodo espera para persistir
-	 *            (salvar) no banco de dados.
-	 * @throws DadoInvalidoException
-	 * @throws DadoSemIdException
-	 * @throws IdDuplicadoException
+	 * @return Cliente/'connection' do CouchDB
 	 */
-	public void salvar(Pessoa dado) throws DadoInvalidoException, DadoSemIdException, IdDuplicadoException {
-
-		if (dado != null) {
-			if (dado.get_id() != null) {
-				if (!existe(dado.get_id())) {
-
-					/**
-					 * Este metodo, ao tentar salvar o objeto, retorna uma
-					 * Response (resposta http do banco de dados para a
-					 * solicitacao). No entanto, ela nao interessa para esta
-					 * aplicacao.
-					 */
-					dbClient.save(dado);
-
-				} else {
-					throw new IdDuplicadoException();
-				}
-
-			} else {
-				throw new DadoSemIdException();
-			}
-		} else {
-			throw new DadoInvalidoException();
-		}
-
-	}
-
-	/**
-	 * Metodo que realiza a busca dos dados cadastrados no BD de acordo com o
-	 * _id passado
-	 * 
-	 * @param id
-	 *            _id do objeto buscado
-	 * @return objeto do tipo Dado encontrado
-	 * @throws DadoInexistenteException
-	 *             Excecao lancada quando nao existe dado no banco com o id
-	 *             passado
-	 */
-	public Pessoa buscarPeloID(String id) throws DadoInexistenteException {
-
-		if (!existe(id)) {
-			throw new DadoInexistenteException();
-		}
-
-		Pessoa dado = dbClient.find(Pessoa.class, id);
-		return dado;
-
-	}
-
-	/**
-	 * Metodo que verifica no banco a existencia de um documento com o ID
-	 * 
-	 * @param id
-	 *            ID do dado a ser verificado
-	 * @return Se ja existe ou nao um documento ja cadastrado com o ID passado
-	 */
-
-	public boolean existe(String id) {
-
-		return dbClient.contains(id);
-
+	public CouchDbClient getCouchDbClient() {
+		return this.dbClient;
 	}
 
 }
